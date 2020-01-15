5f90d49b35a26d795e0b2ac4ad53f8b6430babec
awehrer <awehrer@knights.ucf.edu>
2020-01-14 21:12:43 -0500

    Update jsonConversion.js

--- jsonConversion.js
+++ jsonConversion.js
@@ -11,62 +11,73 @@ function translateMissionCode(missionCode)
         return missionCode;
 }
 
+function translateItemCode(itemCode, itemJson)
+{
+    if (itemJson[itemCode] != undefined)
+        return itemJson[itemCode];
+    else
+        return itemCode;
+}
+
 function convertJSON()
 {
     var jsonObj = JSON.parse(document.getElementById("jsonText").innerHTML);
-
-    /* Example:
-    {{Questheader
-    |Difficulty = 20 |AP = 5 |CC = 400 |Master EXP = 64 |Magical girl EXP = 24 |Bond EXP = 40
-    }}
-    */
-    var questheader = "{{Questheader"
-                    + "\n|Difficulty = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.difficulty
-                    + " |AP = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.ap
-                    + " |CC = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.riche
-                    + " |Master EXP = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.exp
-                    + " |Magical girl EXP = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.cardExp
-                    + " |Bond EXP = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.baseBondsPt
-                    + "\n}}";
-
-    /* Example:
-    {{Questbody
-    |11={{Enemy|Kotori|Dark|24000|2|||5000|6000}}
-    |12={{Enemy|Kotori|Forest|24000|3|||5000|6000}}
-    |21={{Enemy|Shin|Dark|1400000|1|b||11000|15000|{{Hitspot|2=y|5=y|7=y|9=y}}}}
-    }}
-    */
-    var questbody = "{{Questbody"
-                + "\n}}";
-
-    /* Example:
-    {{Missions
-    |Mission1 = Clear when total remaining HP is 30% or more
-    |Mission2 = Clear within 25 turns
-    |Mission3 = Clear without Continue
-    |Mission Reward Quantity = 2
-    |Mission Reward = Lucky Rake
-    }}
-    */
-    var missionRewardCode = jsonObj.webData.userQuestBattleResultList[0].questBattle.missionRewardCode;
-    console.log(missionRewardCode);
-    var index = missionRewardCode.lastIndexOf("_");
-    var missionRewardQuantity = missionRewardCode.substr(index+1);
-    var missions = "{{Missions\n|Mission1 = " + translateMissionCode(jsonObj.webData.userQuestBattleResultList[0].questBattle.mission1)
-                            + "\n|Mission2 = " + translateMissionCode(jsonObj.webData.userQuestBattleResultList[0].questBattle.mission2)
-                            + "\n|Mission3 = " + translateMissionCode(jsonObj.webData.userQuestBattleResultList[0].questBattle.mission3)
-                            + "\n|Mission Reward Quantity = " + missionRewardQuantity
-                            + "\n|Mission Reward = " + missionRewardCode.substr(0, index)
+    
+    $.getJSON("characters.json", convertJSONWithCharacterJSON);
+    
+    function convertJSONWithCharacterJSON(characterJson)
+    {
+        $.getJSON("items.json", convertJSONWithItemJSON);
+        
+        function convertJSONWithItemJSON(itemJson)
+        {
+            /* Example:
+            {{Questheader
+            |Difficulty = 20 |AP = 5 |CC = 400 |Master EXP = 64 |Magical girl EXP = 24 |Bond EXP = 40
+            }}
+            */
+            var questheader = "{{Questheader"
+                            + "\n|Difficulty = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.difficulty
+                            + " |AP = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.ap
+                            + " |CC = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.riche
+                            + " |Master EXP = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.exp
+                            + " |Magical girl EXP = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.cardExp
+                            + " |Bond EXP = " + jsonObj.webData.userQuestBattleResultList[0].questBattle.baseBondsPt
                             + "\n}}";
 
-    document.getElementById("resultText").innerHTML = questheader + "\n" + questbody + "\n" + missions;
+            /* Example:
+            {{Questbody
+            |11={{Enemy|Kotori|Dark|24000|2|||5000|6000}}
+            |12={{Enemy|Kotori|Forest|24000|3|||5000|6000}}
+            |21={{Enemy|Shin|Dark|1400000|1|b||11000|15000|{{Hitspot|2=y|5=y|7=y|9=y}}}}
+            }}
+            */
+            var questbody = "{{Questbody"
+                        + "\n}}";
 
-    var characters;
-    $.getJSON("characters.json", function(json) {
-        characters = json;
-        console.log(characters['1001']);
-    });
-    console.log(characters['1001']);
+            /* Example:
+            {{Missions
+            |Mission1 = Clear when total remaining HP is 30% or more
+            |Mission2 = Clear within 25 turns
+            |Mission3 = Clear without Continue
+            |Mission Reward Quantity = 2
+            |Mission Reward = Lucky Rake
+            }}
+            */
+            var missionRewardCode = jsonObj.webData.userQuestBattleResultList[0].questBattle.missionRewardCode;
+            console.log(missionRewardCode);
+            var index = missionRewardCode.lastIndexOf("_");
+            var missionRewardQuantity = missionRewardCode.substr(index+1);
+            var missions = "{{Missions\n|Mission1 = " + translateMissionCode(jsonObj.webData.userQuestBattleResultList[0].questBattle.mission1)
+                                    + "\n|Mission2 = " + translateMissionCode(jsonObj.webData.userQuestBattleResultList[0].questBattle.mission2)
+                                    + "\n|Mission3 = " + translateMissionCode(jsonObj.webData.userQuestBattleResultList[0].questBattle.mission3)
+                                    + "\n|Mission Reward Quantity = " + missionRewardQuantity
+                                    + "\n|Mission Reward = " + translateItemCode(missionRewardCode.substr(0, index), itemJson)
+                                    + "\n}}";
+
+            document.getElementById("resultText").innerHTML = questheader + "\n" + questbody + "\n" + missions;
+        }
+    }
     
     <!--https://magireco.fandom.com/api.php?action=expandtemplates&format=json&text=%7B%7BTemplate%3ADrops%7CStanding%20Ear%20Wrapper%7D%7D-->
     /*var apiEndpoint = "https://magireco.fandom.com/api.php";
